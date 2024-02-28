a450343e66e3fc6d4825dd460cb43f31165b33f2
John P <johnrprutton@gmail.com>
2024-02-28 18:11:39 +0200

    feat: move data-access -> adapters/database/, implement all repositories in domain

---
+++ packages/adapters/database/src/index.ts
@@ -0,0 +1,164 @@
+import type { Auth, Database } from "@repo/domain/adapters"
+import type { Project, Session, User } from "@repo/domain/entities"
+
+let projects: Project.Entity[] = []
+
+var sessions: Session.Entity[] = []
+var users: User.Entity[] = []
+
+const authRepository: Auth.Repository = {
+  async deleteExpiredSessions() {
+    sessions = sessions.filter(
+      (session) => session.expiresAt.getTime() > Date.now()
+    )
+  },
+
+  async deleteSession(sessionId) {
+    sessions = sessions.filter((session) => session.id !== sessionId)
+  },
+
+  async deleteUserSessions(userId) {
+    sessions = sessions.filter((sessions) => sessions.userId !== userId)
+  },
+
+  async getSessionAndUser(sessionId) {
+    const session = sessions.find((session) => session.id === sessionId)
+    if (!session) return [null, null]
+
+    const user = users.find((user) => user.id === session.userId)
+    if (!user) return [null, null]
+
+    return [session, user]
+  },
+
+  async getUserSessions(userId) {
+    return sessions.filter((session) => session.userId === userId)
+  },
+
+  async setSession(session) {
+    sessions.push(session)
+  },
+
+  async updateSessionExpiration(sessionId, expiresAt) {
+    const session = sessions.find((session) => session.id === sessionId)
+
+    if (session) session.expiresAt = expiresAt
+  }
+}
+
+const userRepository: User.Repository = {
+  async create(user) {
+    const prevUser = users.findIndex((user) => user.username)
+    if (prevUser !== -1)
+      return {
+        success: false,
+        error: {
+          code: "NOT_ALLOWED",
+          message: "Username already in use"
+        }
+      }
+
+    users.push(user)
+
+    return {
+      success: true,
+      data: undefined
+    }
+  },
+
+  async getByUsername(username) {
+    const user = users.find((user) => user.username === username)
+    if (!user)
+      return {
+        success: false,
+        error: {
+          code: "NOT_FOUND",
+          message: "User not found"
+        }
+      }
+
+    return {
+      success: true,
+      data: user
+    }
+  }
+}
+
+const projectRepository: Project.Repository = {
+  async create(newProject) {
+    const project: Project.Entity = {
+      ...newProject,
+      id: projects.length + newProject.name.replaceAll(" ", "_")
+    }
+
+    projects.push(project)
+
+    return {
+      success: true,
+      data: project
+    }
+  },
+
+  async getAll() {
+    return {
+      success: true,
+      data: projects
+    }
+  },
+
+  async getById(projectId) {
+    const result = projects.find((project) => project.id === projectId)
+
+    if (!result)
+      return {
+        success: false,
+        error: {
+          code: "NOT_FOUND",
+          message: "No project found with id '" + projectId + "'"
+        }
+      }
+
+    return {
+      success: true,
+      data: result
+    }
+  },
+
+  async delete(projectId) {
+    const index = projects.findIndex((p) => p.id === projectId)
+    if (index === -1)
+      return {
+        success: false,
+        error: {
+          code: "NOT_FOUND",
+          message: "No projects found with that ID."
+        }
+      }
+
+    projects.splice(index, 1)
+
+    return {
+      success: true,
+      data: undefined
+    }
+  },
+
+  async transact(txfn) {
+    const _projects = projects
+    projects = Array.from(projects)
+
+    const result = await txfn(projectRepository)
+
+    if (result.success === false) {
+      projects = _projects
+    }
+
+    return result
+  }
+}
+
+export const db: Database.Repository = {
+  project: projectRepository,
+  user: userRepository,
+  auth: authRepository
+}
