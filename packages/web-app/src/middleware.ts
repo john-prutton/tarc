372b67eb4e4e940491b7c1c637cd5cac1aaadcc8
John P <johnrprutton@gmail.com>
2024-03-07 15:11:34 +0200

    fix: soft auth check in middleware, db calls throwing error

--- packages/web-app/src/middleware.ts
+++ packages/web-app/src/middleware.ts
@@ -1,10 +1,16 @@
+import { cookies } from "next/headers"
 import { NextRequest, NextResponse } from "next/server"
 
-import { tryGetAuthedUser } from "./components/auth/actions"
+import { Session } from "@repo/domain/entities"
+
+function checkAuthToken() {
+  const sessionId = cookies().get(Session.COOKIE_NAME)?.value
+  return !!sessionId
+}
 
 export async function middleware(req: NextRequest) {
-  const user = await tryGetAuthedUser()
-  if (!user) return NextResponse.redirect(new URL("/", req.url))
+  const hasAuthToken = checkAuthToken()
+  if (!hasAuthToken) return NextResponse.redirect(new URL("/", req.url))
 }
 
 export const config = {
